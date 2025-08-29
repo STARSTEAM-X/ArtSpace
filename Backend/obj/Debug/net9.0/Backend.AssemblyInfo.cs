a5c4b46f65be0d7c96801a0b3504c4014910b6b8
STARSTEAM_X <STARSTEAM097097@gmail.com>
2025-08-30 05:40:39 +0700

    update

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84f07ddde44f6da4c2f7a9bb67f760531483fd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8fdce59253cf990657754ad0011717bf392518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
